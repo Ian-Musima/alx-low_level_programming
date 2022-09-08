befcc02ae449b8e192bbc362a355e94bab161549
Musima-Suto <sutomusima@gmail.com>
2022-09-08 19:34:57 +0000

    Print using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ *main  - Print shown  statement
+ *
+ *Description: Use puts to print
+ *Return: Always 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multiligual puzzle");
+	return (0);
+}
