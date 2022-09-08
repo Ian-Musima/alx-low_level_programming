8f04986fb07a2a69e76c3eeace83d59a81f2c4d9
Musima-Suto <sutomusima@gmail.com>
2022-09-08 20:34:39 +0000

    Fix checker error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	long long int long_log_int;
 	float float_type;
 
-	puts("Size of a char: %lu byte(s)", sizeof(char));
-	puts("Size of an int: %lu byte(s)", sizeof(int));
-	puts("Size of a long int: %lu byte(s)", sizeof(long int));
-	puts("Size of a long long int: %lu byte(s)", sizeof(long long int));
-	puts("Size of a float: %lu byte(s)", sizeof(float));
+	printf("Size of a char: %lu byte(s)", sizeof(char));
+	printf("Size of an int: %lu byte(s)", sizeof(int));
+	printf("Size of a long int: %lu byte(s)", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)", sizeof(float));
 
 	return (0);
 
