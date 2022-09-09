6e711abe79139061195b47edfd35dbc36bb63269
Musima-Suto <sutomusima@gmail.com>
2022-09-09 18:35:26 +0000

    Print base 10 digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - print all base 10 digits
+ *
+ *Return:0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n")
+	return (0);
+}
