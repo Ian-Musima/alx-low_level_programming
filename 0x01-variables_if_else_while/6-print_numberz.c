ef52a462c069a682a70bfd57c22142856167881c
Musima-Suto <sutomusima@gmail.com>
2022-09-09 18:47:04 +0000

    Print base 10 digits with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - print all base 10 digits
+ *
+ *Return:0
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
