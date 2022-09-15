2c9183fad640cf54ce48452b0fdfa9fdc2c691ec
Musima-Suto <sutomusima@gmail.com>
2022-09-15 11:34:03 +0000

    Print alphabet in lowercase ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - print alphabet in lowercase 10 times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i, j = 0;
+
+	while (j < 10)
+	{
+		for (i = 97; i < 123; i++)
+		{
+			_putchar(i);
+		}
+		j++;
+	}
+	_putchar('\n');
+}
