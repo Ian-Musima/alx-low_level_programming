bfee0f4af9848f824de629252e2d0056f9fb3298
Musima-Suto <sutomusima@gmail.com>
2022-09-17 09:54:00 +0000

    Fix trailing whitespace

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,17 +1,18 @@
-#include "main.h"
+#include"main.h"
 
 /**
- *_abs - function computes the absolute value of an integer
- *@int: the integer to be computed
- *
- *Return: Always 0
- */
-int _abs(int i)
+*_abs - function computes the absolute value
+*of an integer
+*@i: the integer to be computed
+*
+*Return: Always 0
+*/
+int_abs(int i)
 {
-	if (i < 0)
-	{
-		return (i *= -1);
-	}
-	else
-		return (i);
+if (i < 0)
+{
+	return (i *= -1);
+}
+else
+	return (i);
 }
