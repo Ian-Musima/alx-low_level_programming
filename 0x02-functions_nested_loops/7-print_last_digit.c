29de3fea33365a3ec0271c42adebce7efef55d79
Musima-Suto <sutomusima@gmail.com>
2022-09-18 09:21:43 +0000

    Find the last digit in a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_last_digit - prints the last digit
+ *@i: the integer to be computed
+ *
+ *Return: the value of the last digit
+ */
+int print_last_digit(int i)
+{
+
+	int last_digit;
+
+	if (i < 0)
+		i = -i;
+
+	last_digit = i % 10;
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
+}
