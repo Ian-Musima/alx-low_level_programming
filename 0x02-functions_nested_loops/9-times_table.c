196ee83d9a033b57eff35155b27ec6c10282c90d
Musima-Suto <sutomusima@gmail.com>
2022-09-18 16:57:52 +0000

    Create times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *times_table - prints the 9 times table, starting with 0
+ *
+ *Return: Always 0.
+ */
+void times_table(void)
+{
+	int i, j, answer;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			answer = i * j;
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (answer >= 10)
+			{
+				_putchar((answer / 10) + '0');
+				_putchar((answer % 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar((answer % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
