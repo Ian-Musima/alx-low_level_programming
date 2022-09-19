dd4ff45435a156e17c4854a2c082d7b45c296d5e
Musima-Suto <sutomusima@gmail.com>
2022-09-19 22:44:18 +0000

    Reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_rev - function reverses string
+ *@s: string to be reversed
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	do {
+		i++;
+
+	} while (s[i] != '\0');
+	do {
+		_putchar(s[i]);
+		i--;
+
+	} while (i >= 0);
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_array - print an array
+ *@a:array
+ *@n:number of elements
+ *
+ */
+void print_array(int *a, int n)
+{
+	int a[];
+
+	do{
+		printf(a[])
+	}
+}
