c0b02db2aa5e3efec6fd725763ec1c7afd6857a7
Musima-Suto <sutomusima@gmail.com>
2022-09-19 07:33:10 +0000

    Find sum of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *add - finds sum of two integers
+ *@i:first integer
+ *@j:second integer
+ *
+ *Return:the result of the sum
+ */
+int add(int i, int j)
+{
+	int answer;
+
+	answer = i + j;
+	return (answer);
+}
