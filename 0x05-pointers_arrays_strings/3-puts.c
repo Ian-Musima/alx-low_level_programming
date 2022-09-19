5db23487e6b8ee58d014d1b2c6e5464370a82d75
Musima-Suto <sutomusima@gmail.com>
2022-09-19 21:59:24 +0000

    Fix checker error, second attempt

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,11 +6,12 @@
  */
 void _puts(char *str)
 {
+	int i = 0;
 
 	do {
-		_putchar(*str);
-		str++;
+		_putchar(str[i]);
+		i++;
 
-	} while (*str != '\0');
+	} while (str[i] != '\0');
 	_putchar('\n');
 }
