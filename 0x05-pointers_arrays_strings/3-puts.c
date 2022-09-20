56e32d234c0d30289af053f09145cb33c8f439f2
Musima-Suto <sutomusima@gmail.com>
2022-09-20 08:45:58 +0000

    Fix empty string error

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,12 +6,11 @@
  */
 void _puts(char *str)
 {
-	int i = 0;
+	int i;
 
-	do {
+	for (i = 0; str[i] != '\0'; i++)
+	{
 		_putchar(str[i]);
-		i++;
-
-	} while (str[i] != '\0');
+	}
 	_putchar('\n');
 }
