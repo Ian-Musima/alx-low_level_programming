d521e3cf31a2250c14680d4e5a3182e2bea00ada
Musima-Suto <sutomusima@gmail.com>
2022-09-20 20:15:44 +0000

    Fix output error, fourth attempt

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,11 +10,13 @@ void puts_half(char *s)
 	int i = 0, j, new_length;
 
 	while (s[i] != '\0')
+	{
 		i++;
+	}
 
 	if (i % 2 == 0)
 		new_length = i / 2;
-	else if (i % 2)
+	else if (i % 2 == 1)
 		new_length = (i - 1) / 2;
 
 	for (j = new_length; j < i; j++)
