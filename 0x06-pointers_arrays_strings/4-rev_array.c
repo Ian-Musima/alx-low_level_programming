f31118489e07188a66f75da5253657a134e87b88
Musima-Suto <sutomusima@gmail.com>
2022-09-23 00:22:01 +0000

    Convert all lowercase to upper

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *reverse_array - reverses the content of an array of 
+ *integers
+ *@a:array to be reversed
+ *@n:number of elements to reverse
+ *
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (a[i] <= n)
+	{
+		i++;
+	}
+	for (j = 0; i != 0:; j++ && i--)
+	{
+		a[j] = a[i] 
+	}
+}
