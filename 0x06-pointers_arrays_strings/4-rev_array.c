f75ab9da74242f5c3689f0c1f26ae8fab301dedd
Musima-Suto <sutomusima@gmail.com>
2022-09-26 09:48:46 +0000

    Reverse the elements of an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- *reverse_array - reverses the content of an array of 
+ *reverse_array - reverses the content of an array of
  *integers
  *@a:array to be reversed
  *@n:number of elements to reverse
  *
  */
-void print_array(int *a, int n)
+void reverse_array(int *a, int n)
 {
-	int i = 0;
+	int i, j, k;
 
-	while (a[i] <= n)
+	for (i = 0; i < n ; i++)
 	{
-		i++;
-	}
-	for (j = 0; i != 0:; j++ && i--)
-	{
-		a[j] = a[i] 
+		for (j = 0; j < i; j++)
+		{
+			k = *(a + i);
+			*(a + i) = *(a + j);
+			*(a + j) = k;
+		}
 	}
 }
