c003ae152924f58af74b67f43e9f888ec36f5de7
Musima-Suto <sutomusima@gmail.com>
2022-09-27 07:16:06 +0000

    Fix linter error

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,9 +2,9 @@
 
 /**
  *_memset - fills memory with a constant byte
- *s: pointer to the memory area
- *b:the constant byte
- @n:size of bytes to be filled
+ *@s: pointer to the memory area
+ *@b:the constant byte
+ *@n:size of bytes to be filled
  *
  *Return: a pointer to the memory area s.
  */
@@ -12,10 +12,9 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++ )
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
 	return (s);
-
 }
