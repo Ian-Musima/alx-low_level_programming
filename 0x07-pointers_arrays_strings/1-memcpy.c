38eca8d25b9b870fa5ce9152536eeee83cb57d1b
Musima-Suto <sutomusima@gmail.com>
2022-09-27 07:46:48 +0000

    Copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_memcpy - copies memory area
+ *@dest:destination of memory area copied
+ *@src:source of memory ara to be copied
+ *@n:number of bytes to be copied
+ *
+ *Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
