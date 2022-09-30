20cf08ea3e30a368f21bbe3efbb5bebce7d17d38
Musima-Suto <sutomusima@gmail.com>
2022-09-30 22:07:45 +0000

    Add all arguements

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - adds positive numbers
+ *@argc: arguement counter
+ *@argv: arguement vector
+ *
+ *Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int add = 0, i;
+
+	if ((argc - 1) > 0)
+	{
+		for ( i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) - atoi(argv[i]) != 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				add += add + atoi(argv[i]);
+				printf("%d", add);
+			}
+
+		}
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
+}
