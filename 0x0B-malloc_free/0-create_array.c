9c1ba491b836f30a8e5c9d34d596e83a44b368a6
Musima-Suto <sutomusima@gmail.com>
2022-10-04 09:17:53 +0000

    Fix segmentation fault, second attempt

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,14 +16,14 @@ char *create_array(unsigned int size, char c)
 	char *p;
 	unsigned int i;
 
-	p = malloc(size * sizeof(char));
+	p = (char *) malloc(size * sizeof(char));
 
 	if (size > 0)
 	{
 		for (i = 0; i < size; i++)
 			p[i] = c;
 	}
-	else if (size == 0 || !p)
+	else
 		return (NULL);
 	return (p);
 }
