d3176d65f80796b7bd347dfecdeaa0a8974b6b3a
Musima-Suto <sutomusima@gmail.com>
2022-10-07 20:32:34 +0000

    Fix output error

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = malloc(sizeof(size) * nmemb);
+	p = malloc(size * nmemb);
 
 	if (p == NULL)
 		return (NULL);
