cb35377f4dc1257b957ad5517686930d45ab72d2
Musima-Suto <sutomusima@gmail.com>
2022-10-07 20:28:33 +0000

    Fix betty error

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,11 +3,11 @@
 
 /**
  *_calloc - allocate memory for an array, using malloc.
- *nmemb - number of elements.
- *size:size in bytes of each element.
+ *@nmemb: number of elements.
+ *@size:size in bytes of each element.
  *
  *Return:pointer to allocated memory. NULL if malloc fails
- or if either of the parameters is equal to 0.
+ *or if either of the parameters is equal to 0.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
