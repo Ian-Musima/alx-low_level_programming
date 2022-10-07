8022a85c7a410753fc133cd6adddc48ae53a7c45
Musima-Suto <sutomusima@gmail.com>
2022-10-07 19:47:20 +0000

    Fix output error, third attempt

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,34 +19,35 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	unsigned int i = 0, j;
-	unsigned int k, l = 0;
+	unsigned int i = 0, j, l = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
+
 	while (s1[l] != '\0')
 		l++;
+
 	while (s2[i] != '\0')
 		i++;
+
 	if (n >= i)
 		n = i;
 
-	p = malloc(sizeof(char) * (l + i + 1));
+	p = malloc(sizeof(char) * (l + n + 1));
 
 	if (p == NULL)
 		return (NULL);
 
-	for (j = 0; s1[j] != '\0'; j++)
+	for (j = 0; j < (l + n); j++)
 	{
-		p[j] = s1[j];
+		if (j < l)
+			p[j] = s1[j];
+		else
+			p[j] = s2[j - l];
 	}
 
-	for (k = 0; k < n; k++)
-	{
-		p[j++] = s2[k];
-	}
 	return (p);
 }
