df32ba7e1b1381bbccd47e388f31440fa596a11f
Musima-Suto <sutomusima@gmail.com>
2022-10-14 19:36:02 +0000

    Print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ *print_numbers - prints numbers, followed by a new line
+ *@separator: string to be printed between numbers
+ *@n:number of integers passed to the function
+ *
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int j;
+
+	va_list args;
+
+	va_start(args, n);
+
+	if (separator == NULL)
+		return;
+
+	for (i = 0; i < n; i++)
+	{
+		j = va_arg(args, int);
+		printf("%d", j);
+		if (i != (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(args);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,9 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *seperattor, const unsigned int n, ...);
+void print_numbers(const char *seperator, const unsigned int n, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
