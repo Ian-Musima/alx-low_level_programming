ce6e3e6e23eb2e5cd82f40c020ee00360dd65ce0
Musima-Suto <sutomusima@gmail.com>
2022-10-15 18:12:41 +0000

    Fix checker error

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,14 +17,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	if (separator == NULL)
-		return;
-
 	for (i = 0; i < n; i++)
 	{
 		j = va_arg(args, int);
 		printf("%d", j);
-		if (i != (n - 1))
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
