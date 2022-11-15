01d14e8bae1b8104a719d64b235247f3e54f2bfe
Ian Musima <sutomusima@gmail.com>
2022-11-15 19:10:11 +0000

    Calculate pow, recursively

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_pow_recursion -calculate value of x raised to power y
+ *@x:number to be raised
+ *@y:power to be raised by
+ *
+ *Return: value or -1 if y < 0.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y >= 0)
+	{
+		if (y == 1)
+			return (x);
+		return (x * _pow_recursion(x, y - 1));
+	}
+	else
+		return (-1);
+}
