221e7082bb5ce1587c313856cdf434934980367e
Ian Musima <sutomusima@gmail.com>
2022-11-19 18:18:12 +0000

    Calculate product

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *mul - evaluates product of two integers
+ *@a:first integer
+ *@b:second integer
+ *
+ *Return:Product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
