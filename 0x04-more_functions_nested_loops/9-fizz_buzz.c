58e839e8147d5610abf018ac47316629a148745b
Ian Musima <sutomusima@gmail.com>
2022-11-21 07:51:15 +0000

    Fizz-Buzz-Test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ *main -"Fizz-Buzz-test"
+ *
+ *Return:0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("%s ", "FizzBuzz");
+			continue;
+		}
+		else if (i % 3 == 0)
+		{
+			printf("%s ", "Fizz");
+			continue;
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s ", "Buzz");
+			continue;
+		}
+		printf("%d ", i);
+	}
+	return (0);
+}
