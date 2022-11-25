ff3f6ee2dc0508da8e79fae4310d118e38bc2431
Ian Musima <sutomusima@gmail.com>
2022-11-25 09:48:45 +0000

    Checker correction, fifth attempt

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,6 +13,9 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i;
 
+	if (!*needle)
+		return (haystack)
+
 	while (*haystack)
 	{
 		i = 0;
@@ -21,7 +24,7 @@ char *_strstr(char *haystack, char *needle)
 		{
 			while (haystack[i] == needle[i])
 			{
-				if (needle[i] == '\0')
+				if (needle[i + 1] == '\0')
 					return (haystack);
 				i++;
 			}
